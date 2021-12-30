d11f0f0706d81294596eb1db00b2279f2b5b2022
Putter Tiatragul <stiatragul@live.com>
2021-12-30 11:07:02 +1100

    script to update gsutil

--- bucket.ps1
+++ bucket.ps1
@@ -1,1 +1,17 @@
-gsutil -m rsync -d -r maba.org.au/public gs://maba.org.au
+# Delete public folder and its contents
+rm -r public/
+
+# Regenerate our static files
+hugo
+
+# Move into our public directory
+cd public/
+
+# -m for multi-thread/process
+# -r for recursively copy (child directories/files)
+
+gsutil -m rsync -d -r . gs://maba.org.au
+
+## Go back to 
+
+cd ..
